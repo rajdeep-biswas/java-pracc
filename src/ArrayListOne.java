81a5a6c464ec79ea5d4a8731d6eb3a3a27581012
Biswas <rajdeep.biswas@sap.com>
2021-11-09 10:47:35 +0530

    commented arraylistone

--- src/ArrayListOne.java
+++ src/ArrayListOne.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+// https://www.hackerrank.com/challenges/java-arraylist/problem
 public class ArrayListOne {
 
     public static void main(String[] args) {
@@ -9,16 +10,22 @@ public class ArrayListOne {
         int n = in.nextInt();
         in.nextLine();
         
+        // An ArrayList of ArrayList of Integers
         ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
         
         for(int i = 0; i < n; i++) {
             
             String lineIn = in.nextLine();
+            
+            // This is how we do a String.split() in Java (it takes regex).
             String[] strArr = lineIn.split("\\s");
             
+            // An ArrayList of Integers
             ArrayList<Integer> intList = new ArrayList<Integer>();
             
+            // This starts from 1 because of the stupid problem spec
             for(int j = 1; j < strArr.length; j++) {
+            	System.out.println("Here: " + strArr[j]);
                 intList.add(Integer.parseInt(strArr[j]));
             }
             
